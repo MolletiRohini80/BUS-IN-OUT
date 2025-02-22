d8c4f1ea4486a4eb277454f68d1be0c6e5933709
Molleti Rohini <119840376+MolletiRohini80@users.noreply.github.com>
2025-02-22 19:48:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,39 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+
+/** @type {import('vite').UserConfig} */
+export default ({ command, mode }) => {
+  const root = process.cwd()
+  const folderName = path.basename(root)
+  // append folder name to base path if using GitHub/GitLab repo pages
+  const usingRepoPages = !process.env.VITE_ROOT_PAGES
+  const base = mode === 'production' && usingRepoPages ? /${folderName}/ : '/'
+
+  console.log('Configuring with...', {
+    command,
+    mode,
+    usingRepoPages,
+    root,
+    folderName,
+    base
+  })
+
+  return defineConfig({
+    root,
+    base:"/BUS-IN-OUT",
+    mode,
+    plugins: [react()],
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true
+    }
+   })
+}
 export default defineConfig({
   plugins: [react()],
   base: "/MolletiRohini80/",
 })
+
